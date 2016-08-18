6afdd00f6dd66f66e44f6f3451c94b4ccaf36f56
Sidney Velado <Mickey@Sidneys-MacBook.local>
2016-08-17 22:22:55 -0600

    Incomplete Version

---
+++ init.py
@@ -0,0 +1,15 @@
+# Main application and route file
+from flask import Flask, render_template
+
+
+
+mysite = Flask(__name__)
+ 
+@mysite.route('/')
+def main():
+	print('Launching home screen ')
+	return render_template('/main.html')
+
+if __name__ == '__main__':
+	mysite.run()
+
